bb0db04e40b79b813cb1b369557e3810b1fb16a4
vikky-6265 <vikkypatankar18@gmail.com>
2026-06-09 13:41:18 +0530

    Add files via upload

---
+++ Code1.java
@@ -0,0 +1,12 @@
+
+public class Code1{
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        int a = 10, b = 20;
+        int c = a + b;
+        System.out.println("Sum is " + c);
+    }
+}
+
+
